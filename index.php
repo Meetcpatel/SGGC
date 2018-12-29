7c047608c6b974987b1ab181be3fd8f694c2cd60
Meet Patel <meetcpatel906@gmail.com>
2018-12-29 12:08:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,9 @@ Name <input type="text" name="t_name" id="t_name"/></br>
 Education <input type="text" name="t_education" id="t_education"/></br>  
 E-mail address <input type="text" name="t_email" id="t_email"/></br>  
 <input type="submit" name="submit" value="Submit" />  
+
+<script type="text/javascript" src="http://fsi110inp7.embed.talkiforum.com/embed/1.js"></script><div style="font-size:80%; text-align:center;" id="fsi110inp7t4lk1prm0">get your own <a href="http://talkiforum.com?utm_source=install&utm_medium=link&utm_campaign=get_your_own">embeddable forum</a> with Talki</div>
+
 </form>  
 <?php  
 /*Connect using SQL Server authentication.*/  
